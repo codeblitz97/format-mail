09c419fc09a408acd43ddf63fced73c131165f69
Mohtasim Alam Sohom <94981761+codeblitz97@users.noreply.github.com>
2023-06-26 18:43:25 +0600

    Add some "cool" functions

--- source/email/htmlTags.ts
+++ source/email/htmlTags.ts
@@ -187,6 +187,12 @@ export const HTMLTags = {
       styleString ? ' style="' + styleString + '"' : ''
     }/>`;
   },
+  /**
+   * Generates a paragraph tag with optional styles.
+   * @param {string} str - The content of the h3 tag.
+   * @param {...string} style - Optional styles to be applied.
+   * @returns {string} The generated paragraph tag HTML.
+   */
   paragraph(str: string, ...style: string[]) {
     if (typeof str !== 'string') {
       throwError(
@@ -199,6 +205,12 @@ export const HTMLTags = {
       style.length > 0 ? style.map((s: string) => s + ';').join(' ') : '';
     return `<p${styleString ? ' style="' + styleString + '"' : ''}>${str}</p>`;
   },
+  /**
+   * Generates a div tag with optional styles.
+   * @param {string} str - The content of the h3 tag.
+   * @param {...string} style - Optional styles to be applied.
+   * @returns {string} The generated div tag HTML.
+   */
   div(str: string, ...style: string[]) {
     if (typeof str !== 'string') {
       throwError(
@@ -212,5 +224,191 @@ export const HTMLTags = {
     return `<div${
       styleString ? ' style="' + styleString + '"' : ''
     }>${str}</div>`;
+  },
+  /**
+   * Generates a span tag with optional styles.
+   * @param {string} str - The content of the span tag.
+   * @param {...string} style - Optional styles to be applied.
+   * @returns {string} The generated span tag HTML.
+   */
+  span(str: string, ...style: string[]) {
+    if (typeof str !== 'string') {
+      throwError(
+        'Custom',
+        'InvalidSpanType',
+        `Expected type of 'STR' to be 'string' but got ${getType(str)} instead.`
+      );
+    }
+    const styleString =
+      style.length > 0 ? style.map((s: string) => s + ';').join(' ') : '';
+    return `<span${
+      styleString ? ' style="' + styleString + '"' : ''
+    }>${str}</span>`;
+  },
+
+  /**
+   * Generates an unordered list (ul) tag with optional styles.
+   * @param {string[]} items - The array of list items.
+   * @param {...string} style - Optional styles to be applied.
+   * @returns {string} The generated ul tag HTML.
+   */
+  ul(items: string[], ...style: string[]) {
+    const itemTags = items.map((item) => `<li>${item}</li>`).join('');
+    const styleString =
+      style.length > 0 ? style.map((s: string) => s + ';').join(' ') : '';
+    return `<ul${
+      styleString ? ' style="' + styleString + '"' : ''
+    }>${itemTags}</ul>`;
+  },
+
+  /**
+   * Generates an ordered list (ol) tag with optional styles.
+   * @param {string[]} items - The array of list items.
+   * @param {...string} style - Optional styles to be applied.
+   * @returns {string} The generated ol tag HTML.
+   */
+  ol(items: string[], ...style: string[]) {
+    const itemTags = items.map((item) => `<li>${item}</li>`).join('');
+    const styleString =
+      style.length > 0 ? style.map((s: string) => s + ';').join(' ') : '';
+    return `<ol${
+      styleString ? ' style="' + styleString + '"' : ''
+    }>${itemTags}</ol>`;
+  },
+
+  /**
+   * Generates a horizontal rule (hr) tag with optional styles.
+   * @param {...string} style - Optional styles to be applied.
+   * @returns {string} The generated hr tag HTML.
+   */
+  hr(...style: string[]) {
+    const styleString =
+      style.length > 0 ? style.map((s: string) => s + ';').join(' ') : '';
+    return `<hr${styleString ? ' style="' + styleString + '"' : ''} />`;
+  },
+
+  /**
+   * Generates a line break (br) tag.
+   * @returns {string} The generated br tag HTML.
+   */
+  br() {
+    return `<br />`;
+  },
+
+  /**
+   * Generates a table (table) tag with optional styles.
+   * @param {string} content - The content of the table.
+   * @param {...string} style - Optional styles to be applied.
+   * @returns {string} The generated table tag HTML.
+   */
+  table(content: string, ...style: string[]) {
+    if (typeof content !== 'string') {
+      throwError(
+        'Custom',
+        'InvalidParagraphType',
+        `Expected type of 'STR' to be 'string' but got ${getType(
+          content
+        )} instead.`
+      );
+    }
+
+    const styleString =
+      style.length > 0 ? style.map((s: string) => s + ';').join(' ') : '';
+    return `<table${
+      styleString ? ' style="' + styleString + '"' : ''
+    }>${content}</table>`;
+  },
+
+  /**
+   * Generates a table row (tr) tag with optional styles.
+   * @param {string} content - The content of the table row.
+   * @param {...string} style - Optional styles to be applied.
+   * @returns {string} The generated tr tag HTML.
+   */
+  tr(content: string, ...style: string[]) {
+    if (typeof content !== 'string') {
+      throwError(
+        'Custom',
+        'InvalidParagraphType',
+        `Expected type of 'STR' to be 'string' but got ${getType(
+          content
+        )} instead.`
+      );
+    }
+
+    const styleString =
+      style.length > 0 ? style.map((s: string) => s + ';').join(' ') : '';
+    return `<tr${
+      styleString ? ' style="' + styleString + '"' : ''
+    }>${content}</tr>`;
+  },
+
+  /**
+   * Generates a table data (td) tag with optional styles.
+   * @param {string} content - The content of the table data.
+   * @param {...string} style - Optional styles to be applied.
+   * @returns {string} The generated td tag HTML.
+   */
+  td(content: string, ...style: string[]) {
+    if (typeof content !== 'string') {
+      throwError(
+        'Custom',
+        'InvalidParagraphType',
+        `Expected type of 'STR' to be 'string' but got ${getType(
+          content
+        )} instead.`
+      );
+    }
+
+    const styleString =
+      style.length > 0 ? style.map((s: string) => s + ';').join(' ') : '';
+    return `<td${
+      styleString ? ' style="' + styleString + '"' : ''
+    }>${content}</td>`;
+  },
+
+  /**
+   * Generates a table header (th) tag with optional styles.
+   * @param {string} content - The content of the table header.
+   * @param {...string} style - Optional styles to be applied.
+   * @returns {string} The generated th tag HTML.
+   */
+  th(content: string, ...style: string[]) {
+    if (typeof content !== 'string') {
+      throwError(
+        'Custom',
+        'InvalidParagraphType',
+        `Expected type of 'STR' to be 'string' but got ${getType(
+          content
+        )} instead.`
+      );
+    }
+
+    const styleString =
+      style.length > 0 ? style.map((s: string) => s + ';').join(' ') : '';
+    return `<th${
+      styleString ? ' style="' + styleString + '"' : ''
+    }>${content}</th>`;
+  },
+
+  /**
+   * Generates a button tag with optional styles and onClick event handler.
+   * @param {string} text - The text content of the button.
+   * @param {Object} options - The options for the button.
+   * @param {Function} options.onClick - The onClick event handler function.
+   * @param {...string} style - Optional styles to be applied.
+   * @returns {string} The generated button tag HTML.
+   */
+  button(text: string, options: { onClick: Function }, ...style: string[]) {
+    const onClickHandler = options.onClick
+      ? `onclick="${options.onClick.toString()}"`
+      : '';
+
+    const styleString =
+      style.length > 0 ? style.map((s: string) => s + ';').join(' ') : '';
+
+    return `<button${onClickHandler}${
+      styleString ? ' style="' + styleString + '"' : ''
+    }>${text}</button>`;
   },
 };
